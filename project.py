89c9d152ad845301b02c222813f4be16e778c134
Anaswara K U <anaswaraku17@gmail.com>
2026-06-05 10:13:45 +0530

    refactor: enhance ExpenseTracker class with input validation and structure

--- project.py
+++ project.py
@@ -1,8 +1,44 @@
 import sys
 
+class ExpenseTracker:
+
+    def __init__(self,date,amount,transaction_type,purpose):
+        self.date = date
+        self.amount = amount
+        self.transaction_type = transaction_type
+        self.purpose = purpose
+
+        if self.type not in ['spend','recieve']:
+            raise ValueError("Type can be either Spend/Receive")
 def main():
-    print("hello")
-    
+
+    if len(sys.argv)==1:
+        sys.exit("Send Request")
+    elif sys.argv[1] in ['add','search','update']:
+            pass
+    else:
+        raise ValueError("Invalid Request")
+
+    request_map={
+        "add": add_expense}
+
+def add_expense():
+    expense = ExpenseTracker(
+        date=input(),
+        amount=input(),
+        transaction_type=input(),
+        purpose=input(),
+    )
+    return expense
+
+
+def search_expense():
+    pass
+
+
+def update_expense():
+    pass
+
 
 if __name__=='__main__':
     main()
