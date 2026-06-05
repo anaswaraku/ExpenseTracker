202a3413ff474659abeb0bb10200eeec1da98f36
Anaswara K U <anaswaraku17@gmail.com>
2026-06-05 09:34:14 +0530

    remove ExpenseTracker class and command line argument validation; add expenses.csv file

--- project.py
+++ project.py
@@ -1,15 +1,8 @@
 import sys
 
-class ExpenseTracker:
-    
-
 def main():
-    try:
-        if sys.argv[1] in ['add','search','report']:
-            pass
-        else:
-            sys.exit("Invalid command line argument")
-
+    print("hello")
+    
 
 if __name__=='__main__':
     main()
